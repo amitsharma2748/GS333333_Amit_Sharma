3ba89ce3b8d1cd318be95f016175afa1629766a1
amitsharma2748 <sharma670amit@gmail.com>
2025-03-10 00:40:25 +0530

    dev configured route for planning module

--- src/components/RouterComponent.tsx
+++ src/components/RouterComponent.tsx
@@ -5,6 +5,7 @@ import IndexStore from '../modules/store/IndexStore'
 import PageNotFound from './PageNotFound'
 import { paths } from '../utils/path'
 import IndexSku from '../modules/sku/IndexSku'
+import IndexPlanning from '../modules/planning/IndexPlanning'
 
 const RouterComponent = () => {
   return (
@@ -12,6 +13,7 @@ const RouterComponent = () => {
       <Route path="/" element={<SideNavHeaderComponent />} >
         <Route path={paths.store} element={<IndexStore/>}/>
         <Route path={paths.sku} element={<IndexSku/>}/>
+        <Route path={paths.planning} element={<IndexPlanning/>}/>
         <Route path="*" element={<PageNotFound/>}/>
       </Route>
       {/* <SideNavHeaderComponent/> */}
---
+++ src/modules/planning/IndexPlanning.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexPlanning = () => {
+  return (
+    <div>IndexPlanning</div>
+  )
+}
+
+export default IndexPlanning
